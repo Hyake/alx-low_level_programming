c58d48ac19d46042ae9ae22250d091292a27e5ad
FRANCIS <122996379+Hyake@users.noreply.github.com>
2023-03-08 15:18:47 -0800

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
- *
+ * _puts_recursion - check the code for Holberton School students.
+ * @s: character of string
+ * Return: Always 0.
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*(s) != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
+	if (*(s) == '\0')
 		_putchar('\n');
 }
